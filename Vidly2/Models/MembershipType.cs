ad5ec45bcabe7af270f2a5a6a41f34538bb82b9d
Unknown <wasikartur87@gmail.com>
2018-01-23 22:08:31 +0100

    Validation

--- Vidly2/Models/MembershipType.cs
+++ Vidly2/Models/MembershipType.cs
@@ -15,5 +15,8 @@ namespace Vidly2.Models
 		public byte DiscountRate { get; set; }
 		[StringLength(255)]
 		public string Name { get; set; }
+
+		public static readonly byte Unknown = 0;
+		public static readonly byte PayAsYouGo = 1;
 	}
 }
